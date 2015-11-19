f3d4d3fbcd1d96570d7ff770e97c43274f732d4c
Sacha Cohen-Scali <sacha.cohen-scali13@imperial.ac.uk>
2015-11-19 18:23:53 +0000

    starting the server in the backend

--- app/components/upload/uploadCtrl.js
+++ app/components/upload/uploadCtrl.js
@@ -1,8 +1,25 @@
 (function (){
   'use strict';
   var adosaur = angular.module('adosaur')
-  adosaur.controller('uploadCtrl', ['$scope', function ($scope) {
-    
+  adosaur.controller('uploadCtrl', ['$scope', 'Upload', '$timeout', function ($scope, Upload, $timeout) {
+    $scope.uploadPic = function(file) {
+	    file.upload = Upload.upload({
+	      url: 'upload',
+	      data: {file: file, username: $scope.username},
+	    });
+
+	    file.upload.then(function (response) {
+	      $timeout(function () {
+	        file.result = response.data;
+	      });
+	    }, function (response) {
+	      if (response.status > 0)
+	        $scope.errorMsg = response.status + ': ' + response.data;
+	    }, function (evt) {
+	      // Math.min is to fix IE which reports 200% sometimes
+	      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+	    });
+    }
   }]);
 }());
   
--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 /*global angular, console*/
 (function () {
   'use strict';
-  var adosaur = angular.module('adosaur', ['ui.router']);
+  var adosaur = angular.module('adosaur', ['ui.router', 'ngFileUpload']);
   adosaur.controller('indexCtrl', ['$scope', function ($scope) {
     
   }]);
