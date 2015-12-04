f48d9c91454378b8f5f061a7b499999f1007d5e9
Sacha Cohen-Scali <sacha-cs@users.noreply.github.com>
2015-12-04 16:52:52 +0000

    Fix possible bug in DataFormatter

---
+++ cnn/AudioCNN.py
@@ -0,0 +1,119 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import lasagne
+import DataFormatter
+import cPickle
+
+def buildCNN(inputVar=None):
+
+  nonlin = lasagne.nonlinearities.rectify
+
+  network = lasagne.layers.InputLayer(shape=(None, 1, 10000), input_var=inputVar)
+
+  network = lasagne.layers.Conv1DLayer(
+                      network, num_filters=20, filter_size=10, stride=0, pad=1,
+                      nonlinearity=nonlin,
+                      W=lasagne.init.GlorotUniform())
+
+  network = lasagne.layers.MaxPool1DLayer(network, pool_size=2)
+
+  network = lasagne.layers.DenseLayer(network, num_units=20, nonlinearity=nonlin)
+  network = lasagne.layers.DenseLayer(network, num_units=63, nonlinearity=nonlin)
+
+  return network
+
+
+# Define number of epochs
+numEpochs = 500
+# Get data and targets from our DataFormatter
+trainingX, trainingY, developmentX, developmentY, testX, testY = DataFormatter.buildAudioData("../rawData/RawAudio/wav/")
+
+# print "Loading training..."
+# f = file('../pickledData/trainingX.save', 'rb')
+# trainingX = cPickle.load(f)
+# f.close()
+#
+# f = file('../pickledData/trainingY.save', 'rb')
+# trainingY = cPickle.load(f)
+# f.close()
+#
+# print "Loading development..."
+# f = file('../pickledData/developmentX.save', 'rb')
+# developmentX = cPickle.load(f)
+# f.close()
+#
+# f = file('../pickledData/developmentY.save', 'rb')
+# developmentY = cPickle.load(f)
+# f.close()
+#
+# print "Loading test..."
+# f = file('../pickledData/testX.save', 'rb')
+# testX = cPickle.load(f)
+# f.close()
+#
+# f = file('../pickledData/testY.save', 'rb')
+# testY = cPickle.load(f)
+# f.close()
+
+# Prepare Theano variables for inputs and targets
+inputVar = T.tensor3('inputs')
+targetVar = T.ivector('targets')
+
+# Create CNN
+network = buildCNN(inputVar)
+
+# Create a loss expression for training
+prediction = lasagne.layers.get_output(network)
+loss = lasagne.objectives.squared_error(prediction, targetVar)
+loss = loss.mean()
+
+# Create update expression for training
+params = lasagne.layers.get_all_params(network, trainable=True)
+updates = lasagne.updates.sgd(loss, params, learning_rate=0.01)
+
+# Create a loss expression for validation and testing
+testPrediction = lasagne.layers.get_output(network, deterministic=True)
+testLoss = lasagne.objectives.categorical_crossentropy(testPrediction,
+                                                        targetVar)
+testLoss = testLoss.mean()
+
+# Create an expression for classification accuracy
+testAcc = T.mean(T.eq(T.argmax(testPrediction, axis=1), targetVar),
+                  dtype=theano.config.floatX)
+
+# Compile a function performing training step and returning corresponsing training loss
+trainFn = theano.function([inputVar, targetVar], loss, updates=updates)
+# Compile a function computing the validation loss and accuracy
+valFn = theano.function([inputVar, targetVar], [testLoss, testAcc])
+
+# Launch training loop over epochs
+for epoch in range(numEpochs):
+	# Do a full pass over training data
+	trainErr = 0
+	startTime = time.time()
+	trainErr += trainFn(trainingX, trainingY)
+
+	# Do a full pass over validation data
+	devErr = 0
+	devAcc = 0
+	err, acc = valFn(developmentX, developmentY)
+	devErr += err
+	devAcc += acc
+
+	# Print the result for this epoch
+	print("Epoch {} of {} took {:.3f}s".format(
+	        epoch + 1, numEpochs, time.time() - start_time))
+	print("  training loss:\t\t{:.6f}".format(trainErr))
+	print("  validation loss:\t\t{:.6f}".format(devErr))
+	print("  validation accuracy:\t\t{:.2f} %".format(devAcc))
+
+# After training, compute and print test error
+testErr = 0
+testAcc = 0
+err, acc = valFn(testX, testY)
+testErr += err
+testAcc += acc
+print("Final results:")
+print("  test loss:\t\t\t{:.6f}".format(testErr))
+print("  test accuracy:\t\t{:.2f} %".format(testAcc))
--- cnn/DataFormatter.py
+++ cnn/DataFormatter.py
@@ -3,15 +3,20 @@ import os
 import csv
 import glob
 import cPickle
+import numpy as np
 
 def getAudioData(audioPath):
-	return scipy.io.wavfile.read(audioPath)[1]
+	data = scipy.io.wavfile.read(audioPath)[1]
+	if len(data.shape) == 2:
+		return data[:, 0]
+	else:
+		return data
 
 
 def splitData(dataArray, sizeChunks):
     splittedArray = [dataArray[i:i + sizeChunks] for i in range(0, len(dataArray), sizeChunks)]
     splittedArray.pop()
-    return splittedArray
+    return np.array(splittedArray)
 
 
 def createLabelDict(labelPath):
@@ -19,7 +24,7 @@ def createLabelDict(labelPath):
 	for file in os.listdir(labelPath):
 		csvReader = csv.reader(open(labelPath + file))
 		for label in csvReader:
-			labelDict[file[:-4]] = int(label[0]) 
+			labelDict[file[:-4]] = int(label[0])
 	return labelDict
 
 
@@ -35,11 +40,13 @@ def buildAudioData(rawAudioPath):
 	developmentX, developmentY = buildExamplesAndTargets(devLabelsDict, rawAudioPath)
 	testX, testY               = buildExamplesAndTargets(testLabelsDict, rawAudioPath)
 
-	return trainingX, trainingY, developmentY, developmentY, testX, testY
+	os.chdir(currDir)
+	return trainingX, trainingY, developmentX, developmentY, testX, testY
 
 def buildExamplesAndTargets(dictionary, path):
-	X = []
-	Y = []
+	X = np.empty(shape=(1, 1, 10000))
+	Y = np.empty(shape=(1))
+
 	os.chdir(path)
 
 	for key, value in dictionary.iteritems():
@@ -48,7 +55,36 @@ def buildExamplesAndTargets(dictionary, path):
 			splittedArray = splitData(audioData, 10000)
 			numExamples = len(splittedArray)
 			yLabels = [value] * numExamples
-			X.extend(splittedArray)
-			Y.extend(yLabels)
-
+			print file
+			X = np.concatenate((X,splittedArray.reshape(numExamples, 1, 10000)))
+			Y = np.concatenate((Y, yLabels))
 	return X, Y
+
+# trainingX, trainingY, developmentX, developmentY, testX, testY = buildAudioData('../rawData/RawAudio/wav/')
+#
+#
+# os.chdir('../pickledData/')
+
+# print "Pickling training..."
+# f = file('trainingX.save', 'wb')
+# cPickle.dump(trainingX, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+# f = file('trainingY.save', 'wb')
+# cPickle.dump(trainingY, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+
+# print "Pickling development..."
+# f = file('developmentX.save', 'wb')
+# cPickle.dump(developmentX, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+# f = file('developmentY.save', 'wb')
+# cPickle.dump(developmentY, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+#
+# print "Pickling test..."
+# f = file('testX.save', 'wb')
+# cPickle.dump(testX, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
+# f = file('testY.save', 'wb')
+# cPickle.dump(testY, f, protocol=cPickle.HIGHEST_PROTOCOL)
+# f.close()
