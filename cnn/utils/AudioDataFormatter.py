2b34b34bc319de752a15f2586cde4f28b103ea17
Sacha Cohen-Scali <sacha-cs@users.noreply.github.com>
2015-12-05 15:10:50 +0000

    Add extra zeros to data for 2d convolution - pair program with Tom

--- cnn/utils/AudioDataFormatter.py
+++ cnn/utils/AudioDataFormatter.py
@@ -5,61 +5,66 @@ import glob
 import numpy as np
 
 def getAudioData(audioPath):
-	data = scipy.io.wavfile.read(audioPath)[1]
-	# if two channels are used, take data from the first one
-	if len(data.shape) == 2:
-		data = data[:, 0]
-	# add extra zeros to do 2d convolution
-	# extraZeros = np.array([0] * len(data))
-	# data = np.vstack((data, extraZeros))
-	return data
+  data = scipy.io.wavfile.read(audioPath)[1]
+  # if two channels are used, take data from the first one
+  if len(data.shape) == 2:
+    data = data[:, 0]
+  return data
 
 
 def splitData(dataArray, sizeChunks):
   splitArray = [dataArray[i:i + sizeChunks] for i in range(0, len(dataArray), sizeChunks)]
   # remove the extra array which is not sizeChunks long
   splitArray.pop()
-  return np.array(splitArray)
+  finalSplitArray = np.empty(shape=(len(splitArray), 2, sizeChunks))
+  # add an extra row of zeros so we can do 2D convolution
+  extraZeros = np.array([0] * sizeChunks)
+  i = 0
+  for elem in splitArray:
+	  arrayToAdd = np.vstack((elem, extraZeros))
+	  finalSplitArray[i] = arrayToAdd
+	  i += 1
+  return finalSplitArray
 
 
 def createLabelDict(labelPath):
-	labelDict = {}
-	for file in os.listdir(labelPath):
-		csvReader = csv.reader(open(labelPath + file))
-		for label in csvReader:
-			labelDict[file[:-4]] = int(label[0])
-	return labelDict
+  labelDict = {}
+  for file in os.listdir(labelPath):
+    csvReader = csv.reader(open(labelPath + file))
+    for label in csvReader:
+      labelDict[file[:-4]] = int(label[0])
+  return labelDict
 
 
 def buildAudioData(rawAudioPath):
-	trainLabelsDict = createLabelDict('../../rawData/labels/Training/')
-	devLabelsDict   = createLabelDict('../../rawData/labels/Development/')
-	testLabelsDict  = createLabelDict('../../rawData/labels/Testing/')
+  trainLabelsDict = createLabelDict('../../rawData/labels/Training/')
+  devLabelsDict   = createLabelDict('../../rawData/labels/Development/')
+  testLabelsDict  = createLabelDict('../../rawData/labels/Testing/')
 
-	currDir = os.getcwd()
-	rawAudioPath = currDir + '/' + rawAudioPath
-	trainingX, trainingY       = buildExamplesAndTargets(trainLabelsDict, rawAudioPath)
-	developmentX, developmentY = buildExamplesAndTargets(devLabelsDict, rawAudioPath)
-	testX, testY               = buildExamplesAndTargets(testLabelsDict, rawAudioPath)
+  currDir = os.getcwd()
+  rawAudioPath = currDir + '/' + rawAudioPath
+  trainingX, trainingY       = buildExamplesAndTargets(trainLabelsDict, rawAudioPath)
+  developmentX, developmentY = buildExamplesAndTargets(devLabelsDict, rawAudioPath)
+  testX, testY               = buildExamplesAndTargets(testLabelsDict, rawAudioPath)
 
-	os.chdir(currDir)
-	return trainingX, trainingY, developmentX, developmentY, testX, testY
+  os.chdir(currDir)
+  return trainingX, trainingY, developmentX, developmentY, testX, testY
 
 def buildExamplesAndTargets(dictionary, path):
-	X = np.empty(shape=(1, 1, 10000))
-	Y = np.empty(shape=(1))
+  X = np.empty(shape=(1, 2, 10000))
+  Y = np.empty(shape=(1))
 
-	os.chdir(path)
+  os.chdir(path)
 
-	for key, value in dictionary.iteritems():
-		for file in glob.glob("*" + key + "*.wav"):
-			audioData = getAudioData(path + file)
-			splitArray = splitData(audioData, 10000)
-			numExamples = len(splitArray)
-			yLabels = [value] * numExamples
-			X = np.concatenate((X,splitArray.reshape(numExamples, 1, 10000)))
-			Y = np.concatenate((Y, yLabels))
-	return X, Y
+  for key, value in dictionary.iteritems():
+    for file in glob.glob("*" + key + "*.wav"):
+      audioData = getAudioData(path + file)
+      splitArray = splitData(audioData, 10000)
+      numExamples = len(splitArray)
+      yLabels = [value] * numExamples
+      X = np.concatenate((X,splitArray))
+      Y = np.concatenate((Y, yLabels))
+  return X, Y
 
 
 trainingX, trainingY, developmentX, developmentY, testX, testY = buildAudioData('../../rawData/RawAudio/wav/')
