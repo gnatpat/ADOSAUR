7d6dce8f78e9a785709d2de0dea91e712f86a69d
LeeHyunAh <LeeHyunAh@dyn1190-211.wlan.ic.ac.uk>
2015-12-05 15:15:48 +0000

    Fixed rgbExtractor to pass general path

--- rgbExtractor.py
+++ rgbExtractor.py
@@ -1,5 +1,6 @@
 from PIL import Image
 
-im = Image.open('/Users/LeeHyunAh/Desktop/sacha.png')
-pixels = list(im.getdata())
-print pixels
+def extractRGB(filepath):
+    im = Image.open(filepath)
+    pixels = list(im.getdata())
+    print pixels
