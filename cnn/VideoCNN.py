c14d4f89e29d4fde8d06a34ecb46958b626a44c2
danesherbs <danesherbs@gmail.com>
2015-12-31 20:08:00 +0000

    CNN trains with AVEC data

--- cnn/VideoCNN.py
+++ cnn/VideoCNN.py
@@ -1,10 +1,3 @@
-#
-#  REQUIRES "test.mp4" IN CURRENT DIRECTORY
-#
-#  test.mp4 is currently hard coded as a 1280 x 720 video.
-#  Change these parameters to match your test.mp4
-#
-
 import lasagne
 from lasagne import layers
 from lasagne.updates import nesterov_momentum
@@ -13,65 +6,80 @@ import numpy as np
 import pickle
 
 from utils.VideoUtils import getCNNdata
+from utils.Utils import testCNN
+
+UNITS_ON_BDI = 64
 
 
-# TODO: load actual training, validation and test sets.
-#       Change getCNNdata in Utils.py
+# Loads training, validation and testing data
 def loadData():
-    data = {}  # initialise dictionary
-    testSet = getCNNdata('./test.mp4').reshape(-1, 1, 1280, 720)  # generate test set; -1 forces np to deduce dimension
-    targets = np.array([1] * 11, dtype='int32')  # pseudo targets - NOTE: must have dtype='int32'
+    # retrieve data
+    trainingX, trainingY, validationX, validationY, testingX, testingY = getCNNdata()
 
-    # populate dictionary
-    data['X_train'] = testSet  # a list of grayscale images
-    data['y_train'] = targets  # a list of ints (on BDI scale)
-    data['num_examples_train'] = testSet.shape[0]
-    data['input_shape'] = (None,) + testSet.shape[1:]  # ignore num examples. Here input is 1280 x 720 grayscale image
-    data['output_dim'] = 10  # chosen arbitrarily
+    # initialise and populate dictionary
+    data = {}
+    data['trainingX']   = trainingX    # a list of grayscale images
+    data['trainingY']   = trainingY    # a list of ints (on BDI scale)
+    data['validationX'] = validationX  # a list of grayscale images
+    data['validationY'] = validationY  # a list of ints (on BDI scale)
+    data['testingX']    = testingX     # a list of grayscale images
+    data['testingY']    = testingY     # a list of ints (on BDI scale)
+    data['num_examples_train'] = trainingX.shape[0]
+    data['input_shape'] = (None,) + trainingX.shape[1:]
+    data['output_dim'] = UNITS_ON_BDI  # BDI scale
+
+    # Report number of training examples found
+    print("\n\nGot %i training datasets.\n" % data['num_examples_train'])
 
     return data
 
-def CNN(data):
+def buildCNN(data):
     # small net for proof of concept
     # see http://goo.gl/GZUQOb for more realistic config
-    net2 = NeuralNet(
-    layers=[
-        ('input', layers.InputLayer),
-        ('conv1', layers.Conv2DLayer),
-        ('pool1', layers.MaxPool2DLayer),
-        ('hidden2', layers.DenseLayer),
-        ('output', layers.DenseLayer),
-        ],
-    input_shape=data['input_shape'],
-    conv1_num_filters=5, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
-    hidden2_num_units=50,
-    output_num_units=data['output_dim'],
-    output_nonlinearity=lasagne.nonlinearities.softmax,
+    network = NeuralNet(
+        # architecture
+        layers=[
+            ('input', layers.InputLayer),
+            ('conv1', layers.Conv2DLayer),
+            ('pool1', layers.MaxPool2DLayer),
+            ('hidden2', layers.DenseLayer),
+            ('output', layers.DenseLayer),
+            ],
+        input_shape=data['input_shape'],
+        conv1_num_filters=5, conv1_filter_size=(3, 3), pool1_pool_size=(2, 2),
+        hidden2_num_units=50,
+        output_num_units=data['output_dim'],
+        output_nonlinearity=lasagne.nonlinearities.softmax,
 
-    update_learning_rate=0.01,
-    update_momentum=0.9,
+        # learning parameters
+        update_learning_rate=0.01,
+        update_momentum=0.9,
 
-    regression=False,
-    max_epochs=3,
-    verbose=1,
+        # miscellaneous
+        regression=False,
+        max_epochs=3,
+        verbose=1,
     )
-
-    # Train the network
-    net2.fit(data['X_train'], data['y_train'])
-
 
     # TODO: pickle trained network (after we've optimised)
 
+    return network
 
-    # Try the network on the first training example  -- meaningless, but whatevs
-    print("\nActual:\t\t%s" % str(data['y_train'][0]))
-    print("Predicted:\t%s" % str(net2.predict([data['X_train'][0]])[0]))
+def CNN(data):
+    # build network - need data to determine input/output shapes
+    print("Building network...")
+    network = buildCNN(data)
 
+    # train the network
+    print("Training network...\n")
+    network.fit(data['trainingX'], data['trainingY'])
+
+    return network
 
 def main():
     data = loadData()
-    print("Got %i testing datasets.\n" % len(data['X_train']))
-    CNN(data)
+    network = CNN(data)
+    testCNN(network, data['testingX'], data['testingY'])
 
 if __name__ == '__main__':
     main()
