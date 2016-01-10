7422878039c51f96027edf5aeb8cf917fc178bf4
tombarti <tom.bartissol@gmail.com>
2016-01-10 23:17:41 +0000

    fixed a login issue

--- app/frontend/components/login/loginCtrl.js
+++ app/frontend/components/login/loginCtrl.js
@@ -1,8 +1,18 @@
 (function () {
   'use strict';
   var adosaur = angular.module('adosaur');
-  adosaur.controller('loginCtrl', ['$scope', '$http', '$location', 'Flash',
-    function ($scope, $http, $location, Flash) {
+  adosaur.controller('loginCtrl', ['$scope', 'util', '$location', 'Flash', '$state',
+    function ($scope, util, $location, Flash, $state) {
+      // if user already has a current session then redirect him to his home page
+      util.getCurrentUser(function (user) {
+        if (user.found) {
+          console.log('USER FOUND');
+          $state.go('doctor', {uid: user.user.uid});
+        }
+      }, function (data) {
+        console.log('Ajax request failed');
+      });
+
       var err = $location.search().err,
         errMsg = "";
       if (err === "1") {
