33b2e1cd884af5a8f20ee5ce07ab2185c698ecb7
tombarti <tom.bartissol@gmail.com>
2016-01-14 11:25:25 +0000

    improved patient profile page

--- app/frontend/components/doctor/patientProfileCtrl.js
+++ app/frontend/components/doctor/patientProfileCtrl.js
@@ -1,16 +1,30 @@
 (function () {
   'use strict';
   var adosaur = angular.module('adosaur');
-  adosaur.controller('patientProfileCtrl', ['$scope', 'util', '$stateParams',
-    function ($scope, util, $stateParams) {
+  adosaur.controller('patientProfileCtrl', ['$scope', 'util', '$stateParams', '$http',
+    function ($scope, util, $stateParams, $http) {
       console.log('IN Patient Profile controller');
-
+      var dlevels = {
+        "0": "Minimal depression",
+        "1": "Mild depression",
+        "2": "Moderate depression",
+        "3": "Severe depression",
+        "-1": "Test not done yet"
+      };
+      util.getCurrentUser(function (user) {
+        $scope.doctor = user.user;
+      });
       util.getPatient($stateParams.pid, function (res) {
         $scope.patient = res.user;
         console.log($scope.patient._id);
         util.getPatientTests($scope.patient._id, function (data) {
+          var l = data.tests.length,
+            i = 0;
           $scope.tests = data.tests;
-          console.log($scope.tests);
+          //  get test information
+          for (i; i < l; i += 1) {
+            $scope.tests[i].result = dlevels[$scope.tests[i].result];
+          }
         });
       });
     }]);
--- app/frontend/components/upload/uploadCtrl.js
+++ app/frontend/components/upload/uploadCtrl.js
@@ -1,12 +1,13 @@
-(function (){
+(function () {
   'use strict';
-  var adosaur = angular.module('adosaur')
+  var adosaur = angular.module('adosaur');
   adosaur.controller('uploadCtrl', ['$scope', 'Upload', '$timeout', '$location', function ($scope, Upload, $timeout, $location) {
     var dlevels = {
       "0": "Minimal depression",
       "1": "Mild depression",
       "2": "Moderate depression",
-      "3": "Severe depression"
+      "3": "Severe depression",
+      "-1": "Test not done yet"
     };
     $scope.prediction = dlevels[$location.search().prediction];
 
